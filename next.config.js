e1af8ea76bd59bf3789f3d31feeebccc51ca00e9
Andreas Rissling <a.rissling@steinberg.de>
2023-12-08 11:31:45 +0100

    ...

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+});
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+};
+
+module.exports = withMDX(nextConfig);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,3 @@
-import styles from './page.module.css';
-
 export default function Home() {
-  return (
-    <main>
-      <h1 className={styles.siteTitle}>Site Title</h1>
-      <nav className={styles.navList}>
-        <li>test</li>
-      </nav>
-    </main>
-  );
+  return <p>Test</p>;
 }
